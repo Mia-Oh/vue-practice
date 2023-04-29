ca06e168d4291b5598c1d8b94606ecbb9cb5419b
Mia-Oh <songmiaoh@gmail.com>
2023-04-29 19:11:06 +0900

    style:gif 파일 추가

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <template>
-  <img alt="logo" src="./assets/logo.png">
+  <img alt="logo" class="logo" src="./assets/logo.png">
   <div>
-    <h4>{{products[0]}} 원룸</h4>
-    <p>{{price1}} 만원</p>
-    <span>신고 : {{신고수}}</span>
-    <button @click="신고수+=1">허위 매물 신고</button>
+    <img src="./assets/pil0.gif" class='pil-img'>
+    <h4>{{posts[0]}} 피리미리</h4>
+    <p>{{comments[0]}}</p>
+    <span>{{likeCount[0]}}</span>
+    <button @click="likeCount[0]+=1">❤️</button>
   </div>
   <div>
-    <h4>{{products[1]}} 원룸</h4>
-    <p>{{price2}} 만원</p>
+    <img src="./assets/pil1.gif" class='pil-img'>    
+    <h4>{{posts[1]}} 피리미리</h4>
+    <p>{{comments[1]}}</p>
+    <span>{{likeCount[1]}}</span>
+    <button @click="likeCount[1]+=1">❤️</button>
   </div>
   <div>
-    <h4>{{products[3]}} 원룸</h4>
-    <p>{{price2}} 만원</p>
-  </div>  
+    <img src="./assets/pil2.gif" class='pil-img'>
+    <h4>{{posts[2]}} 피리미리</h4>
+    <p>{{comments[2]}}</p>
+    <span>{{likeCount[2]}}</span>
+    <button @click="likeCount[2]+=1">❤️</button>
+  </div>
 </template>
 
 <script>
@@ -21,10 +28,9 @@ export default {
   name: 'App',
   data() {
     return {
-      신고수 : 0,
-      products : ['역삼동','천호동','남영동'],
-      price1: 60,
-      price2 : 90,
+      likeCount : [0,0,0],
+      posts : ['머찐','여름','깨물하트'],
+      comments: ['너무 멋있어여','아주 사랑스러워요','커아이~'],
     }
   },
   components: {
@@ -41,10 +47,19 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-img {
+.logo {
   width: 10%;
 }
 span {
   margin-right: 1rem;
+}
+button {
+  background: none;
+  border: none;
+  font-size: 1.2rem;
+}
+.pil-img {
+  width: 30rem;
+  margin-top: 100px;
 }
 </style>
