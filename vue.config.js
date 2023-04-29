bdbc27815a290761ee04083dd19bd40aaa44b774
Mia-Oh <songmiaoh@gmail.com>
2023-04-29 20:51:54 +0900

    fix:for deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/vue-practice',
-})
+},
+{
+  publicPath: '/vue-practice/',
+  outputDir: "docs",
+}
+);
