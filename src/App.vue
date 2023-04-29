57eabae01b8b2631494a8ed5230c665669f16ea1
Mia-Oh <songmiaoh@gmail.com>
2023-04-29 18:38:50 +0900

    feat:이벤트 핸들러 추가 연습

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <img alt="logo" src="./assets/logo.png">
   <div>
-    <h4>XX 원룸</h4>
+    <h4>{{products[0]}} 원룸</h4>
     <p>{{price1}} 만원</p>
+    <span>신고 : {{신고수}}</span>
+    <button @click="신고수+=1">허위 매물 신고</button>
   </div>
-    <div>
-    <h4>XX 원룸</h4>
+  <div>
+    <h4>{{products[1]}} 원룸</h4>
     <p>{{price2}} 만원</p>
   </div>
+  <div>
+    <h4>{{products[3]}} 원룸</h4>
+    <p>{{price2}} 만원</p>
+  </div>  
 </template>
 
 <script>
@@ -15,6 +21,8 @@ export default {
   name: 'App',
   data() {
     return {
+      신고수 : 0,
+      products : ['역삼동','천호동','남영동'],
       price1: 60,
       price2 : 90,
     }
@@ -35,5 +43,8 @@ export default {
 }
 img {
   width: 10%;
+}
+span {
+  margin-right: 1rem;
 }
 </style>
